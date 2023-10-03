8cb065620e113db0f20567d84c7db8c006577ea6
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 19:04:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 """
 ETL-Query script
 """
-
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import head_query, insert_query
+import fire
 
-# Extract
-print("Extracting data...")
-extract()
+def main(query_string):
+    # Extract
+    print("Extracting data...")
+    extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+    # Transform and load
+    print("Transforming data...")
+    load()
 
-# Query
-print("Querying data...")
-query()
+    # Query
+    print("Querying data...")
+    query(query_string)
+
+if __name__ == "__main__":
+    fire.Fire(main)
