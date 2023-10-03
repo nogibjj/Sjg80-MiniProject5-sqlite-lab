78f1961c21a6f2230ab1f34a37fc3788dfba3b92
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 00:25:03 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -4,13 +4,15 @@ import sqlite3
 
 
 def query():
-    """Query the database for the top 5 rows of the GroceryDB table"""
+    """Query the database for the top 5 rows of the DB_Email_Names table"""
     conn = sqlite3.connect("DB_Email_Names.db")
     cursor = conn.cursor()
     cursor.execute("SELECT COUNT(*) FROM DB_Email_Names.db")
+    Name_Results = cursor.fetchall()
+    
     print("Top number of observations DB_Email_Names table:")
     print(cursor.fetchall())
     conn.close()
-    return "Success"
+    return Name_Results
 
 
