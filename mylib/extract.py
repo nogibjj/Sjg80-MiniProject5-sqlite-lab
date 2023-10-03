63bcf81a738a03b0957ab0b07288d0b0d2a15236
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-02 22:52:11 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,13 +5,16 @@ Names dataset
 """
 import requests
 
-def extract(url="https://github.com/tokern/piicatcher/blob/aa15f90bd25a45fc7b3ab75c52be71d714b8b57a/tests/samples/sample-data.csv", 
-            file_path="data/DataSet_withNames.csv"):
-    """"Extract a url to a file path"""
+def extract(
+    url="https://github.com/tokern/piicatcher/blob/aa15f90bd25a45fc7b3ab75c52be71d714b8b57a/tests/samples/sample-data.csv",
+    file_path="data/DataSet_withNames.csv",
+):
+    """ "Extract a url to a file path"""
     with requests.get(url) as r:
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             f.write(r.content)
     return file_path
+
 
 
 
