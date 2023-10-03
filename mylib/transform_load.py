837e0ea9ccd1e2ed923a3e4778b09e4bfc96a4e7
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 19:34:33 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,44 +1,34 @@
+
+"""
+Transforms and Loads data into the local SQLite3 database
+Example:
+,general name,count_products,
+ingred_FPro,avg_FPro_products,avg_distance_root,
+ingred_normalization_term,semantic_tree_name,semantic_tree_node
+"""
 import sqlite3
 import csv
 import os
 
 # Load the csv file and insert into a new sqlite3 database
-def load(dataset="data/DataSet_withNames.csv"):
+def load(dataset="../Sjg80-MiniProject5-sqlite-lab/data/DataSet_withNames.csv"):
     """Transforms and Loads data into the local SQLite3 database"""
 
-    # prints the full working directory and path
+     # prints the full working directory and path
     print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=""), delimiter=",")
-    conn = sqlite3.connect("DB_Email_Names.db")
+    payload = csv.reader(
+        open(dataset, newline="", encoding="iso-8859-1"), delimiter=","
+    )
+    conn = sqlite3.connect("NamesDB.db")
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS DB_Email_Names")
+    c.execute("DROP TABLE IF EXISTS NamesDB")
     c.execute(
-        """CREATE TABLE DB_Email_Names (
-            id,
-            gender,
-            birthdate,
-            maiden_name,
-            lname,
-            fname,
-            address,
-            city,
-            state,
-            zip,
-            phone,
-            email,
-            cc_type,
-            cc_number,
-            cc_cvc,
-            cc_expiredate
-        )"""
+        """CREATE TABLE NamesDB 
+        (Rank,Name, Platform, Publisher, Developer, 
+        Critic_Score, User_Score, Total_Shipped, Year)"""
     )
     # insert
-    c.executemany(
-        """INSERT INTO DB_Email_Names VALUES (
-        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-        payload,
-    )
+    c.executemany("INSERT INTO NamesDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
     conn.close()
-    return "DB_Email_Names.db"
-
+    return "Names.db"
