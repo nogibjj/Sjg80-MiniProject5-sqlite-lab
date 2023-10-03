fa5a1cec168e542820a4b3fd7f0053385ff49acb
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 19:19:07 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,12 +4,19 @@ Test goes here
 
 from mylib.query import query
 
+
 def test_query():
-    assert query("SELECT value FROM DataSet_withNames ORDER BY value DESC LIMIT 2") 
-    == [('Jane Doe',), ('John Doe',)]
-    
+    assert query("SELECT value FROM DataSet_withNames ORDER BY value DESC LIMIT 2") == [
+        ("Jane Doe",),
+        ("John Doe",),
+    ]
+
     pass
+
+
 def test_extract():
     pass
+
+
 def test_transform_load():
     pass
