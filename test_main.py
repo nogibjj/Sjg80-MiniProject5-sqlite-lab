bc34287190137607256e914fd8c051ee30dd5888
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 19:15:38 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
 """
 Test goes here
-
 """
 
 from mylib.query import query
 
 def test_query():
-    assert query("SELECT COUNT(*) FROM DB_Email_Names.db") == 31
+    assert query("SELECT value FROM DataSet_withNames ORDER BY value DESC LIMIT 2") == [('Jane Doe',), ('John Doe',)]
+    
+    pass
+def test_extract():
+    pass
+def test_transform_load():
+    pass
