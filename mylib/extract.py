46b4be5a07838225cfe24691f9d63ccb5ea6a61a
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 22:41:10 +0000

    asda

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,13 +5,16 @@ food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://github.com/nogibjj/Sjg80-MiniProject5-sqlite-lab"
+    "/blob/6c2ab2fb3e8c193f60cf683b8668e71ce03d0f89/DataSet_withNames.csv",
+            file_path="data/DataSet_withNames.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
     return file_path
+
+
 
 
 
