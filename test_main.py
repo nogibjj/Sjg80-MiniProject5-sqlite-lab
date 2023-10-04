5bb33f3d9b1b5e9b216d2f3b9c0ddd00f99ff26f
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-03 20:03:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,18 +3,13 @@ Test goes here
 """
 
 from mylib.query import query
-SELECT maiden_name
-FROM your_table_name
-LIMIT 1;
 
 def test_query():
     assert query("SELECT maiden_name FROM NamesDB LIMIT 1") == [ ('Smith',)]
     pass
 
-
 def test_extract():
     pass
-
 
 def test_transform_load():
     pass
